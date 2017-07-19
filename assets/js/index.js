a3bbe4d86206072ddba299b036f17633d404131a
Bonnenu <283026381@qq.com>
2017-07-19 16:07:25 +0800

    设置ul宽度

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,15 +5,16 @@ window.onload=function () {
     var width = $("img").width();
     var height = $('img').height();
     $("li").width(width).height(height);
+    $("ul").width($("img").length*100+"%");
     slide();
     function slide() {
         $("ul").animate({
             left: '-=' + width
         }, 8000, function () {
-            if ($(this).css("left") == -4800+"px") {
+            if ($(this).css("left") == -4*width+"px") {
                 $(this).css("left",0);
             }
             slide();
         })
-    };
+    }
 };
