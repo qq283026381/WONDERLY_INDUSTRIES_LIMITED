9a25cff1cabbbc7348d008adb19d6029e826eb56
Bonnenu <283026381@qq.com>
2017-07-21 10:08:49 +0800

    通过Ajax从数据库取轮播图

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,40 @@
 /**
  * Created by Bonnenu on 2017/7/19.
  */
-window.onload=function () {
+showSlider();
+window.onload = function () {
     initIframeHeight();
     var width = $("img").width();
     var height = $('img').height();
     $("li").width(width).height(height);
-    $("ul").width($("img").length*100+"%");
+    $("ul").width($("img").length * 100 + "%");
     slide();
     function slide() {
         $("ul").animate({
             left: '-=' + width
         }, 8000, function () {
-            if ($(this).css("left") == -4*width+"px") {
-                $(this).css("left",0);
+            if ($(this).css("left") === -4 * width + "px") {
+                $(this).css("left", 0);
             }
             slide();
         })
     }
 };
+function showSlider() {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        // IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {
+        // IE6, IE5 浏览器执行代码
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            document.getElementById("slider").innerHTML = xmlhttp.responseText;
+        }
+    };
+    xmlhttp.open("GET", "util/ajax/getSlider.php", true);
+    xmlhttp.send();
+}
