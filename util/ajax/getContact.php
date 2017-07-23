ca74cb11c7f7117f3cb0f72e048265a5ba4747c8
Bonnenu <283026381@qq.com>
2017-07-23 14:34:02 +0800

    php实现从数据库获取公司联系方式

---
+++ util/ajax/getContact.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bonnenu
+ * Date: 2017/7/23
+ * Time: 12:59
+ */
+require "../mysql/mysql.php";
+$mysql = new Mysql();
+$conn = $mysql->connect();
+$queryAboutContent = "select `content` from contact";
+$result=$conn->query($queryAboutContent);
+if ($result->num_rows > 0) {
+    $row=$result->fetch_array();
+    echo $row['content'];
+}
+$conn->close();
