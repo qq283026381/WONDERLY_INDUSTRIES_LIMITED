809f19da7a73179775458753f4f0cb5f6cb8734d
Bonnenu <283026381@qq.com>
2017-07-23 14:33:24 +0800

    contact页面js

---
+++ assets/js/contact.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Bonnenu on 2017/7/23.
+ */
+showContact();
+window.onload=function () {
+    initIframeHeight();
+};
+function showContact() {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        // IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {
+        // IE6, IE5 浏览器执行代码
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            document.getElementById("address").innerHTML = xmlhttp.responseText;
+        }
+    };
+    xmlhttp.open("GET", "../../util/ajax/getContact.php", true);
+    xmlhttp.send();
+}
