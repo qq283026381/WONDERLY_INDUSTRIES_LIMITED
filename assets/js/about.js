45585afe06680db3dccb4743a37e1b16916768fc
Bonnenu <283026381@qq.com>
2017-07-23 11:04:11 +0800

    通过ajax获取about页面内容

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,6 +1,27 @@
 /**
  * Created by Bonnenu on 2017/7/20.
  */
+showAbout();
 window.onload=function () {
   initIframeHeight();
 };
+
+
+function showAbout() {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        // IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {
+        // IE6, IE5 浏览器执行代码
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            document.getElementById("detail").innerHTML = xmlhttp.responseText;
+        }
+    };
+    xmlhttp.open("GET", "../util/ajax/getAbout.php", true);
+    xmlhttp.send();
+}
