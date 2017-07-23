c1f4fcbecc7ea6670cca5711aaccbde28b2c4426
Bonnenu <283026381@qq.com>
2017-07-23 22:45:22 +0800

    增加设置active类的方法

--- assets/js/common.js
+++ assets/js/common.js
@@ -8,5 +8,9 @@ function initIframeHeight() {
             var bHeight = iframe.contentWindow.document.body.offsetHeight;
             iframe.style.height=bHeight+"px";
         }
-
+}
+function setNavActive(id) {
+    var iframe = document.getElementsByClassName("nav")[0];
+    var about = iframe.contentWindow.document.getElementById(id);
+    about.setAttribute("class","active");
 }
