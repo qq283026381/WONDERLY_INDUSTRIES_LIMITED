32e4b82f6803b1b9dd89c9a4c79d0f0da704113b
Bonnenu <283026381@qq.com>
2017-07-24 13:05:30 +0800

    左侧目录跟随鼠标滚轮移动。当滚轮移动超过nav菜单后，左侧目录开始跟随移动。

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,16 +1,32 @@
 /**
  * Created by Bonnenu on 2017/7/23.
  */
-window.onload=function () {
+window.onload = function () {
     initIframeHeight();
     setNavActive("product");
+    categoryMove();
     $("li").mouseenter(function () {
-        if($(this).children("ul")){
-            $(this).children("ul").css("display","block");
+        if ($(this).children("ul")) {
+            $(this).children("ul").css("display", "block");
         }
     }).mouseleave(function () {
-        if($(this).children("ul")){
-            $(this).children("ul").css("display","none");
+        if ($(this).children("ul")) {
+            $(this).children("ul").css("display", "none");
         }
     })
 };
+
+function categoryMove() {
+    var hHeight = $(".head").height();
+    var nHeight = $(".nav").height();
+    var height = hHeight + nHeight;
+    $(window).scroll(function () {
+        var scrollTop = $(document).scrollTop();
+        if (scrollTop > height) {
+            scrollTop -= height;
+        } else {
+            scrollTop = 0;
+        }
+        $(".left").stop().animate({"top": scrollTop}, 600);
+    });
+}
