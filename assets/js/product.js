ad28b0f1cba1e8be626d22150215e72a1a9e8084
Bonnenu <283026381@qq.com>
2017-07-26 11:18:39 +0800

    通过ajax从数据库获取产品信息（图片）

--- assets/js/product.js
+++ assets/js/product.js
@@ -2,10 +2,10 @@
  * Created by Bonnenu on 2017/7/23.
  */
 showCategory();
+showProduct();
 window.onload = function () {
     initIframeHeight();
     setNavActive("product");
-
     categoryMove();
     showProductDetail();
 };
@@ -25,17 +25,17 @@ function categoryMove() {
     });
 }
 
-function showProductDetail() {
-    $("#Drywall-Screw-WDL-M001A").click(showBox);
+function showProductDetail(id) {
+    getProductDetail(id);
     $("#product-detail-mask").click(hideBox);
     $("#close-box").mouseenter(function () {
-        $(this).fadeOut("fast",function () {
-            $(this).css("background","url('../../assets/imgs/icons/close-hover.png') no-repeat");
+        $(this).fadeOut("fast", function () {
+            $(this).css("background", "url('../../assets/imgs/icons/close-hover.png') no-repeat");
             $(this).fadeIn("fast");
         })
     }).mouseleave(function () {
-        $(this).fadeOut("fast",function () {
-            $(this).css("background","url('../../assets/imgs/icons/close.png') no-repeat");
+        $(this).fadeOut("fast", function () {
+            $(this).css("background", "url('../../assets/imgs/icons/close.png') no-repeat");
             $(this).fadeIn("fast");
         })
     });
@@ -46,40 +46,40 @@ function showProductDetail() {
     $("#product-detail-box").mouseleave(function () {
         $("#product-detail-mask").click(hideBox)
     });
-
-    function hideBox() {
-        $("#product-detail-box").fadeOut(200);
-        $("#product-detail-mask").fadeOut(200);
-    }
-
-    function showBox() {
-        $("#product-detail-mask").fadeIn(500, function () {
-            $("#product-detail-box").fadeIn(300);
-        });
-    }
 }
 
-function showCategory(){
-    var xmlhttp=returnXmlhttp();
+function hideBox() {
+    $("#product-detail-box").fadeOut(200);
+    $("#product-detail-mask").fadeOut(200);
+}
+
+function showBox() {
+    $("#product-detail-mask").fadeIn(500, function () {
+        $("#product-detail-box").fadeIn(300);
+    });
+}
+
+function showCategory() {
+    var xmlhttp = returnXmlhttp();
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-            var newNode=document.createElement("li");
-            newNode.innerHTML=xmlhttp.responseText;
+            var newNode = document.createElement("ul");
+            newNode.innerHTML = xmlhttp.responseText;
             console.log(newNode);
-            var lis=newNode.childNodes;
+            var lis = newNode.childNodes;
             console.log(lis);
-            for(var i=0;i<lis.length;i++){
-                var li=lis[i];
-                li.onmouseover=function () {
-                    var submenu=this.getElementsByTagName("ul");
-                    if(submenu.length){
-                        submenu[0].style.display="block";
+            for (var i = 0; i < lis.length; i++) {
+                var li = lis[i];
+                li.onmouseover = function () {
+                    var submenu = this.getElementsByTagName("ul");
+                    if (submenu.length) {
+                        submenu[0].style.display = "block";
                     }
                 };
-                li.onmouseout=function () {
-                    var submenu=this.getElementsByTagName("ul");
-                    if(submenu.length){
-                        submenu[0].style.display="none";
+                li.onmouseout = function () {
+                    var submenu = this.getElementsByTagName("ul");
+                    if (submenu.length) {
+                        submenu[0].style.display = "none";
                     }
                 }
             }
@@ -87,5 +87,28 @@ function showCategory(){
         }
     };
     xmlhttp.open("GET", "../../util/ajax/getCategory.php", true);
+    xmlhttp.send();
+}
+
+
+function showProduct() {
+    var xmlhttp = returnXmlhttp();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            document.getElementById("productReval").innerHTML = xmlhttp.responseText;
+        }
+    };
+    xmlhttp.open("GET", "../../util/ajax/getProduct.php", true);
+    xmlhttp.send();
+}
+
+function getProductDetail(id) {
+    var xmlhttp = returnXmlhttp();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            document.getElementById("product-detail").innerHTML = xmlhttp.responseText;
+        }
+    };
+    xmlhttp.open("GET", "../../util/ajax/getProductDetail.php?img=" + id, true);
     xmlhttp.send();
 }
