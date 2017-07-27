151371a9cb23f0e4b740bd6364ca08e16fdc77d3
Bonnenu <283026381@qq.com>
2017-07-27 09:43:29 +0800

    从数据库中获取轮播图信息，并自动生成html代码嵌入页面

---
+++ util/ajax-mg/getSliderInfo.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bonnenu
+ * Date: 2017/7/26
+ * Time: 15:04
+ */
+require "../mysql/slider.php";
+$slider = new Slider();
+$content = "";
+$result = $slider->getSliderInfo();
+if ($result->num_rows > 0) {
+    $content .= "<thead><tr><th>优先级</th><th>图片</th><th>预览</th><th>Edit</th><th>Delete</th></tr></thead>";
+    $content .= "<tfoot><tr><td colspan='12'>sdfasffs</td></tr></tfoot>";
+    $content .= "<tbody>";
+    $i = 0;
+    while ($row = $result->fetch_array()) {
+        $index = $row['index'];
+        $img = $row['img'];
+        if ($i % 2 == 0) {
+            $content .= "<tr class='even'>";
+        }else{
+            $content .= "<tr class='odd'>";
+        }
+        $content .= "<td>" . $index . "</td><td>" . $img . "</td><td><img class='slider-overview' src='../../assets/imgs/slider/" . $img . "' alt='" . $img . "' /></td><td><a href='../../util/controllers/ReviseSliderController.php?img=" . $img . "'><img src='../../assets-mg/img/edit.png' border='0'/> </a></td><td><a href='../../util/controllers/DeleteSliderController.php?img=" . $img . "'><img src='../../assets-mg/img/trash.gif' border='0'/> </a></td></tr>";
+        $i++;
+    }
+    $content .= "</tbody>";
+}
+echo $content;
