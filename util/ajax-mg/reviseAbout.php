18d52af9e9aea596483038b02cb866e70728c47d
Bonnenu <283026381@qq.com>
2017-07-27 21:20:59 +0800

    修改about页面的内容

---
+++ util/ajax-mg/reviseAbout.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bonnenu
+ * Date: 2017/7/27
+ * Time: 19:56
+ */
+require "../mysql/about.php";
+$about=new About();
+$content=isset($_POST['about-content'])?$_POST['about-content']:"";
+$result=$about->reviseAbout($content);
+if($result){
+    echo "<script>alert('修改成功！');window.location.href=document.referrer;</script>";
+}else{
+    echo "<script>alert('修改失败！');history.back();</script>";
+}
