a1651c24e5dd1438615ba153513d99fb10705fc7
Bonnenu <283026381@qq.com>
2017-07-27 18:56:08 +0800

    获取单个轮播图详细信息的方法。 删除单个轮播图的方法。 更改单个轮播图的方法。

--- util/mysql/slider.php
+++ util/mysql/slider.php
@@ -22,21 +22,60 @@ class Slider
         $mysql = new Mysql();
         $conn = $mysql->connect();
         $queryAddSlider = "insert into `slider` (`index`,`img`) VALUES ( '" . $index . "' , '" . $img . "' )";
-        $result=$conn->query($queryAddSlider);
-        if($result){
-            $conn->close();
+        $result = $conn->query($queryAddSlider);
+        $conn->close();
+        if ($result) {
             return true;
-        }else{
-            $conn->close();
+        } else {
             return false;
         }
     }
-    function getSliderInfo(){
+
+    function getSliderInfo()
+    {
         $mysql = new Mysql();
         $conn = $mysql->connect();
         $querySlider = "select `index`,`img` from slider order by `index`";
-        $result=$conn->query($querySlider);
+        $result = $conn->query($querySlider);
         $conn->close();
         return $result;
+    }
+
+    function getSliderDetail($img)
+    {
+        $mysql = new Mysql();
+        $conn = $mysql->connect();
+        $querySliderDetail = "select `index`,`img` from slider where `img` = '" . $img . "'";
+        $result=$conn->query($querySliderDetail);
+        $conn->close();
+        return $result;
+    }
+
+    function deleteSlider($img)
+    {
+        $mysql = new Mysql();
+        $conn = $mysql->connect();
+        $queryDeleteSlider = "delete from slider where img = '" . $img . "'";
+        $result = $conn->query($queryDeleteSlider);
+        $conn->close();
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function updateSlider($oldImg, $img, $index)
+    {
+        $mysql = new Mysql();
+        $conn = $mysql->connect();
+        $queryUpdateSlider = "update slider set `img` = '" . $img . "' , `index` = '" . $index . "' where `img` = '" . $oldImg . "'";
+        $result = $conn->query($queryUpdateSlider);
+        $conn->close();
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
