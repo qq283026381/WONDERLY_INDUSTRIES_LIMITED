e05778d4502d33937b12af63c22687e93f86d971
Bonnenu <283026381@qq.com>
2017-07-27 09:43:03 +0800

    添加轮播图图片的php实现。其中添加成功后，返回上一页并刷新使用的window.location.href=document.referrer；

---
+++ util/ajax-mg/addSlider.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bonnenu
+ * Date: 2017/7/27
+ * Time: 8:35
+ */
+require "../mysql/slider.php";
+$index=isset($_POST['sliderIndex'])?$_POST['sliderIndex']:"";
+$file=isset($_FILES['newSliderImg'])?$_FILES['newSliderImg']:"";
+$img=$file['name'];
+if($index!=""&&$img!=""){
+    $slider=new Slider();
+    if($slider->addSlider($index,$img)){
+        echo "<script>alert('添加成功！');window.location.href=document.referrer;</script>";
+    }else{
+        echo "<script>alert('添加失败！');history.back();</script>";
+    }
+}
