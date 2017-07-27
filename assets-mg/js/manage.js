43bd01e1cd6a7d6a9fb606a0fa64ac563a2eff22
Bonnenu <283026381@qq.com>
2017-07-27 18:51:03 +0800

    左侧菜单跟随移动方法。 本想在上传图片的时候检测图片像素是否符合要求，发现不知道如何实现，遂放弃。 添加修改轮播图时候提交后检测图片的方法。 点击单个轮播图修改logo后展示该轮播图详细信息的方法。

--- assets-mg/js/manage.js
+++ assets-mg/js/manage.js
@@ -1,6 +1,47 @@
 window.onload = function () {
     showSliderInfo();
+    slidebarMove();
 };
+
+function slidebarMove() {
+    function $(id) {
+        return document.getElementById(id);
+    }
+    function scroll() {
+        if (window.pageYOffset !== null)  //  ie9+ 和其他浏览器
+        {
+            return {
+                left: window.pageXOffset,
+                top: window.pageYOffset
+            }
+        }
+        else if (document.compatMode === "CSS1Compat")  // 声明的了 DTD
+        // 检测是不是怪异模式的浏览器 -- 就是没有 声明<!DOCTYPE html>
+        {
+            return {
+                left: document.documentElement.scrollLeft,
+                top: document.documentElement.scrollTop
+            }
+        }
+        return { //  剩下的肯定是怪异模式的
+            left: document.body.scrollLeft,
+            top: document.body.scrollTop
+        }
+    }
+    var pic = $("sidebar");
+    var leader = 0;
+    var target = 0;
+    var timer = null;  // 定时器
+    var top = pic.offsetTop;  // 50
+    window.onscroll = function() {
+        clearInterval(timer);
+        target = scroll().top + top;  // 把最新的 scrolltop 给  target
+        timer = setInterval(function() {
+            leader = leader + (target - leader ) / 10;
+            pic.style.top = leader + 'px';
+        },30)
+    }
+}
 
 function returnXmlhttp() {
     var xmlhttp;
@@ -30,7 +71,6 @@ function checkImg() {
     var maxsize = 200 * 1024;//150KB
     var errMsg = "上传的附件文件不能超过200KB！！！";
     var tipMsg = "您的浏览器暂不支持计算上传文件的大小，确保上传文件不要超过200KB，建议使用FireFox、Chrome浏览器。";
-    var sizeMsg = "请上传930*475尺寸的图片！";
     var browserCfg = {};
     var ua = window.navigator.userAgent;
     if (ua.indexOf("MSIE") >= 1) {
@@ -45,26 +85,15 @@ function checkImg() {
         alert("请先选择上传文件");
         return false;
     }
-    var fileSize ;
-    var fileHeight;
-    var fileWidth;
-    var img=new Image();
+    var fileSize = 0;
     if (browserCfg.firefox || browserCfg.chrome) {
         fileSize = obj_file.files[0].size;
-        img.src=obj_file;
     } else if (browserCfg.ie) {
-        var obj_img = document.getElementById('sliderImg');
+        var obj_img = document.getElementById("tempImg");
         obj_img.dynsrc = obj_file.value;
         fileSize = obj_img.filesize;
-        img.src=obj_img;
     } else {
         alert(tipMsg);
-        return false;
-    }
-    fileWidth=img.width;
-    fileHeight=img.height;
-    if (fileWidth !== 930 && fileHeight !== 475) {
-        alert(sizeMsg);
         return false;
     }
     if (fileSize === -1) {
@@ -76,4 +105,89 @@ function checkImg() {
     } else {
         return true;
     }
+}
+
+function checkReviseImg() {
+    var obj_file = document.getElementById("sliderImg");
+    if (obj_file.value === "") {
+        return true;
+    } else {
+        var maxsize = 200 * 1024;//150KB
+        var errMsg = "上传的附件文件不能超过200KB！！！";
+        var tipMsg = "您的浏览器暂不支持计算上传文件的大小，确保上传文件不要超过200KB，建议使用FireFox、Chrome浏览器。";
+        var browserCfg = {};
+        var ua = window.navigator.userAgent;
+        if (ua.indexOf("MSIE") >= 1) {
+            browserCfg.ie = true;
+        } else if (ua.indexOf("Firefox") >= 1) {
+            browserCfg.firefox = true;
+        } else if (ua.indexOf("Chrome") >= 1) {
+            browserCfg.chrome = true;
+        }
+        var fileSize = 0;
+        if (browserCfg.firefox || browserCfg.chrome) {
+            fileSize = obj_file.files[0].size;
+        } else if (browserCfg.ie) {
+            var obj_img = document.getElementById("tempImg");
+            obj_img.dynsrc = obj_file.value;
+            fileSize = obj_img.filesize;
+        } else {
+            alert(tipMsg);
+            return false;
+        }
+        if (fileSize === -1) {
+            alert(tipMsg);
+            return false;
+        } else if (fileSize > maxsize) {
+            alert(errMsg);
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+function showSliderDetail(img) {
+    getSliderDetail(img);
+    $("#revise-mask").click(hideBox);
+    $("#close-box").mouseenter(function () {
+        $(this).fadeOut("fast", function () {
+            $(this).css("background", "url('../../assets/imgs/icons/close-hover.png') no-repeat");
+            $(this).fadeIn("fast");
+        })
+    }).mouseleave(function () {
+        $(this).fadeOut("fast", function () {
+            $(this).css("background", "url('../../assets/imgs/icons/close.png') no-repeat");
+            $(this).fadeIn("fast");
+        })
+    });
+    $("#close-box").click(hideBox);
+    $("#revise-box").mouseenter(function () {
+        $("#revise-mask").unbind()
+    });
+    $("#revise-box").mouseleave(function () {
+        $("#revise-mask").click(hideBox)
+    });
+}
+
+function hideBox() {
+    $("#revise-box").fadeOut(200);
+    $("#revise-mask").fadeOut(200);
+}
+
+function showBox() {
+    $("#revise-mask").fadeIn(500, function () {
+        $("#revise-box").fadeIn(300);
+    });
+}
+
+function getSliderDetail(img) {
+    var xmlhttp = returnXmlhttp();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            document.getElementById("revise-form").innerHTML = xmlhttp.responseText;
+        }
+    };
+    xmlhttp.open("GET", "../../util/ajax-mg/getSliderDetail.php?img="+img, true);
+    xmlhttp.send();
 }
