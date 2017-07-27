26affb2e345f98bdc849491edd70221ea8c3decf
Bonnenu <283026381@qq.com>
2017-07-27 09:46:26 +0800

    判断用户身份的方法，传进来的两个参数均已经过md5加密

---
+++ util/mysql/admin.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bonnenu
+ * Date: 2017/7/26
+ * Time: 13:38
+ */
+
+class Admin
+{
+    /**
+     * Admin constructor.
+     */
+    public function __construct()
+    {
+        require "mysql.php";
+    }
+
+    public function checkAdmin($name, $password)
+    {
+        $mysql = new Mysql();
+        $conn = $mysql->connect();
+        $queryAdmin = "select `name`,`password` from admin where `name`='" . $name . "' and `password` = '" . $password . "' limit 1";
+        $result = $conn->query($queryAdmin);
+        if ($result->num_rows > 0) {
+            $conn->close();
+            return true;
+        }else{
+            $conn->close();
+            return false;
+        }
+    }
+
+}
