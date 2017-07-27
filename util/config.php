5009ca5b0ebfef5f4323ef5371614e86a0a32888
Bonnenu <283026381@qq.com>
2017-07-27 10:36:10 +0800

    定义常量

---
+++ util/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bonnenu
+ * Date: 2017/7/27
+ * Time: 10:24
+ */
+define("SLIDER_PATH","../../assets/imgs/slider/");
+define("CONTACT_PATH","../../assets/imgs/contact/");
