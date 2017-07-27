c7453cd521448470779c4e2985ff3098ffd0d847
Bonnenu <283026381@qq.com>
2017-07-27 10:35:10 +0800

    将文件存储在服务器上

--- util/ajax-mg/addSlider.php
+++ util/ajax-mg/addSlider.php
@@ -11,11 +11,24 @@ $file = isset($_FILES['newSliderImg']) ? $_FILES['newSliderImg'] : "";
 $img = $file['name'];
 if ($index != "" && $img != "") {
     $slider = new Slider();
+    $result = $slider->getSliderInfo();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_array()) {
+            if ($img === $row['img']) {
+                echo "<script>alert('图片已经存在，请重命名或换一张图。');history.back();</script>";
+                exit();
+            }
+        }
+    }
+    require "../file/file.php";
+    require "../config.php";
+    $upload=new File();
+    $upload->uploadJpg($file,constant("SLIDER_PATH"));
     if ($slider->addSlider($index, $img)) {
         echo "<script>alert('添加成功！');window.location.href=document.referrer;</script>";
     } else {
         echo "<script>alert('添加失败！');history.back();</script>";
     }
-}else{
+} else {
     echo "<script>alert('添加失败！');history.back();</script>";
 }
