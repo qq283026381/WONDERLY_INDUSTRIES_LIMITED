2f20f92bfabf9526851315677dc42e3b96e6b1e0
Bonnenu <283026381@qq.com>
2017-07-27 10:35:44 +0800

    删除轮播图php实现。包括从服务器上将文件删除。

---
+++ util/ajax-mg/deleteSlider.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bonnenu
+ * Date: 2017/7/27
+ * Time: 9:49
+ */
+require "../mysql/slider.php";
+require "../file/file.php";
+require "../config.php";
+$img=isset($_GET['img'])?$_GET['img']:"";
+if($img){
+    $slider=new Slider();
+    $file=new File();
+    $file->deleteJpg($img,constant("SLIDER_PATH"));
+    if($slider->deleteSlider($img)){
+        echo "<script>alert('删除成功！');window.location.href=document.referrer;</script>";
+    }else{
+        echo "<script>alert('删除失败！');history.back();</script>";
+    }
+}
