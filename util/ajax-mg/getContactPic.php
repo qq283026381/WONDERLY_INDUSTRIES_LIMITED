045c702fb7a38f0ee37c1a81c364ab45fcd24018
Bonnenu <283026381@qq.com>
2017-07-28 11:27:56 +0800

    获取contact页面的图片

---
+++ util/ajax-mg/getContactPic.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bonnenu
+ * Date: 2017/7/28
+ * Time: 11:00
+ */
+require "../mysql/contact.php";
+require "../config.php";
+$contact = new Contact();
+$result = $contact->getImg();
+if ($result) {
+    $row = $result->fetch_array();
+    $content = "<label>原图片：</label>";
+    $content .= "<img src='";
+    $content.=constant("CONTACT_PATH");
+    $content.=$row['img'];
+    $content.="' />";
+    echo $content;
+}
