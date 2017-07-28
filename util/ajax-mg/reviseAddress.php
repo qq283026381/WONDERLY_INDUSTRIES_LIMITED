e797869a614a513f6785bea0e3b455e863f46091
Bonnenu <283026381@qq.com>
2017-07-28 10:49:40 +0800

    修改地址

---
+++ util/ajax-mg/reviseAddress.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bonnenu
+ * Date: 2017/7/28
+ * Time: 10:43
+ */
+require "../mysql/contact.php";
+$address=isset($_POST['revise-address'])?$_POST['revise-address']:"";
+$contact=new Contact();
+if($contact->reviseAddress($address)){
+    echo "<script>alert('修改成功！');window.location.href=document.referrer+'#contactManage-tab';</script>";
+}else{
+    echo "<script>alert('修改失败！');history.back();</script>";
+}
