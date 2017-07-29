be6dc794ff0b049fb5813d8d84beda552af861ac
Bonnenu <283026381@qq.com>
2017-07-29 14:35:02 +0800

    检查添加的产品图片是否合法

--- assets-mg/js/manage.js
+++ assets-mg/js/manage.js
@@ -116,6 +116,45 @@ function checkImg() {
         return true;
     }
 }
+function checkProductImg() {
+    var maxsize = 200 * 1024;//150KB
+    var errMsg = "上传的附件文件不能超过200KB！！！";
+    var tipMsg = "您的浏览器暂不支持计算上传文件的大小，确保上传文件不要超过200KB，建议使用FireFox、Chrome浏览器。";
+    var browserCfg = {};
+    var ua = window.navigator.userAgent;
+    if (ua.indexOf("MSIE") >= 1) {
+        browserCfg.ie = true;
+    } else if (ua.indexOf("Firefox") >= 1) {
+        browserCfg.firefox = true;
+    } else if (ua.indexOf("Chrome") >= 1) {
+        browserCfg.chrome = true;
+    }
+    var obj_file = document.getElementById("productImg");
+    if (obj_file.value === "") {
+        alert("请先选择上传文件");
+        return false;
+    }
+    var fileSize = 0;
+    if (browserCfg.firefox || browserCfg.chrome) {
+        fileSize = obj_file.files[0].size;
+    } else if (browserCfg.ie) {
+        var obj_img = document.getElementById("tempImg");
+        obj_img.dynsrc = obj_file.value;
+        fileSize = obj_img.filesize;
+    } else {
+        alert(tipMsg);
+        return false;
+    }
+    if (fileSize === -1) {
+        alert(tipMsg);
+        return false;
+    } else if (fileSize > maxsize) {
+        alert(errMsg);
+        return false;
+    } else {
+        return true;
+    }
+}
 
 function checkReviseImg() {
     var obj_file = document.getElementById("sliderImg");
