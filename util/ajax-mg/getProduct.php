3a959c6ad8ea48c1c080c7a9f42334eeba33eda0
Bonnenu <283026381@qq.com>
2017-07-29 13:44:18 +0800

    获取产品，并按菜单顺序排列

---
+++ util/ajax-mg/getProduct.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bonnenu
+ * Date: 2017/7/29
+ * Time: 13:08
+ */
+require "../mysql/product.php";
+require "../mysql/menu.php";
+require "../config.php";
+$product = new Product();
+$menu = new Menu();
+$menuResult = $menu->getMenu();
+$content = "";
+$i = 0;
+if ($menuResult) {
+    while ($menuItem = $menuResult->fetch_array()) {
+        $submenuResult = $menu->getSubmenuByParent($menuItem['item']);
+        if ($submenuResult) {
+            while ($submenuItem = $submenuResult->fetch_array()) {
+                $productResult = $product->getProductByCategory($submenuItem['item']);
+                if ($productResult) {
+                    while ($productItem = $productResult->fetch_array()) {
+                        $index = $productItem['index'];
+                        $img = $productItem['img'];
+                        if ($i % 2 == 0) {
+                            $content .= "<tr class='even'>";
+                        } else {
+                            $content .= "<tr class='odd'>";
+                        }
+                        $content .= "<td>" . $index . "</td>";
+                        $content .= "<td>" . $submenuItem['item'] . "</td>";
+                        $content .= "<td>" . $img . "</td>";
+                        $content .= "<td><img src='" . constant("PRODUCT_PATH") . $menuItem['item'] ."/". $submenuItem['item'] . "/" . $img . "' /></td>";
+                        $content .= "<td><a><img src=\"../../assets-mg/img/edit.png\" border=\"0\"> </a></td>";
+                        $content .= "<td><a><img src=\"../../assets-mg/img/trash.gif\" border=\"0\"> </a></td>";
+                        $content .= "</tr>";
+                        $i++;
+                    }
+                }
+            }
+        }
+    }
+    echo $content;
+}
