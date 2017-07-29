04809ba80c33ce7e966fd0f829e769727780cf0f
Bonnenu <283026381@qq.com>
2017-07-29 13:45:02 +0800

    添加产品时子类的获取。为避免用户随意输入，从数据库取出子类类别

---
+++ util/ajax-mg/getProductSubmenu.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bonnenu
+ * Date: 2017/7/29
+ * Time: 12:42
+ */
+$parent = $_GET['parent'];
+require "../mysql/menu.php";
+$menu = new Menu();
+$result = $menu->getSubmenuByParent($parent);
+$content = "";
+if ($result) {
+    while ($row = $result->fetch_array()) {
+        $item = $row['item'];
+        $content .= "<option value='" . $item . "'>" . $item . "</option>";
+    }
+    echo $content;
+}
