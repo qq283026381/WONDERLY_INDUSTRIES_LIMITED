86d4bdae109eee6d86cd2f54e44499eef575bdd8
Bonnenu <283026381@qq.com>
2017-07-29 11:37:57 +0800

    删除submenu的方法

---
+++ util/ajax-mg/deleteSubmenu.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bonnenu
+ * Date: 2017/7/29
+ * Time: 11:37
+ */
+require "../mysql/menu.php";
+$item = isset($_GET['item']) ? $_GET['item'] : "";
+$menu = new Menu();
+if ($menu->deleteSubmenu($item)) {
+    echo "<script>alert('删除成功！');window.location.href=document.referrer+'#categoryManage-tab';</script>";
+} else {
+    echo "<script>alert('删除失败！');history.back();</script>";
+}
