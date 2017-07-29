7134ff8d2e2bcff718466dc0041c8e1f657f5f50
Bonnenu <283026381@qq.com>
2017-07-29 10:08:37 +0800

    获取单个轮播图信息后，将form表头用php输出。以便所有的修改都可以使用这个弹窗。

--- util/ajax-mg/getSliderDetail.php
+++ util/ajax-mg/getSliderDetail.php
@@ -12,6 +12,8 @@ $content = "";
 $result = $slider->getSliderDetail($img);
 if ($result->num_rows > 0) {
     $row = $result->fetch_array();
+    $content.="<form id=\"revise-form\" class=\"form\" action=\"../../util/ajax-mg/reviseSlider.php\"
+                  enctype=\"multipart/form-data\" method=\"post\" onsubmit=\"return checkReviseImg();\">";
     $content .= "<div class='form_row'>";
     $content .= "<label for='reviseSliderIndex' >优先级：</label>";
     $content .= "<input type='number' min='1' autocomplete='off' required placeholder='输入 1 以上的整数' value='" . $row['index'] . "' class='form_input' name='reviseSliderIndex' id='reviseSliderIndex' />";
@@ -22,6 +24,6 @@ if ($result->num_rows > 0) {
     $content .= "<img class='revise-img' src='../../assets/imgs/slider/" . $row['img'] . "' />";
     $content .= "</div>";
     $content .= "<div class=\"form_row\"><label for=\"reviseSliderImg\">新图片：</label><input type=\"file\" name=\"reviseSliderImg\" id=\"reviseSliderImg\"/></div>
-<div class=\"form_row\"><input type=\"submit\" class=\"form_submit\" value=\"更改\"/></div><div class=\"clear\"></div>";
+<div class=\"form_row\"><input type=\"submit\" class=\"form_submit\" value=\"更改\"/></div><div class=\"clear\"></div></form>";
 }
 echo $content;
