af335d03b256040dc8042ec1de5fe8c3b9e12c56
Bonnenu <283026381@qq.com>
2017-07-29 10:13:17 +0800

    通过item查询menu方法。 新增nemu方法。 删除menu方法。

--- util/mysql/menu.php
+++ util/mysql/menu.php
@@ -27,12 +27,42 @@ class Menu
         return $result;
     }
 
+    function getMenuByItem($item)
+    {
+        $mysql = new Mysql();
+        $conn = $mysql->connect();
+        $queryMenuByItem = "select `item`,`index` from menu where `item` = '" . $item . "'";
+        $result = $conn->query($queryMenuByItem);
+        $conn->close();
+        return $result;
+    }
+
     function getSubmenuByParent($parent)
     {
         $mysql = new Mysql();
         $conn = $mysql->connect();
         $querySubmenuByParent = "select `index`,`item` from submenu where `parent` = '" . $parent . "' order by `index`";
-        $result=$conn->query($querySubmenuByParent);
+        $result = $conn->query($querySubmenuByParent);
+        $conn->close();
+        return $result;
+    }
+
+    function addMenu($index, $item)
+    {
+        $mysql = new Mysql();
+        $conn = $mysql->connect();
+        $queryAddMenu = "insert into menu (`index`,`item`) values ('" . $index . "','" . $item . "')";
+        $result = $conn->query($queryAddMenu);
+        $conn->close();
+        return $result;
+    }
+
+    function deleteMenu($item)
+    {
+        $mysql = new Mysql();
+        $conn = $mysql->connect();
+        $queryDeleteMenu = "delete from menu where `item` = '" . $item . "'";
+        $result=$conn->query($queryDeleteMenu);
         $conn->close();
         return $result;
     }
