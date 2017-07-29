70bba87c5e39288c82658100c69a808294a8c972
Bonnenu <283026381@qq.com>
2017-07-29 10:07:07 +0800

    获取menu，并生成html代码嵌入页面

--- util/ajax-mg/getMenu.php
+++ util/ajax-mg/getMenu.php
@@ -21,34 +21,10 @@ if ($menuResult) {
         }
         $content .= "<td>" . $index . "</td>";
         $content .= "<td>" . $item . "</td>";
-        $content .= "<td>无</td>";
-        $content .= "<td><a><img src=\"../../assets-mg/img/edit.png\" border=\"0\"> </a></td>";
-        $content .= "<td><a><img src=\"../../assets-mg/img/trash.gif\" border=\"0\"> </a></td>";
+        $content .= "<td><a><img onclick='showBox();showMenuDetail(\"" . $item . "\");' src=\"../../assets-mg/img/edit.png\" border=\"0\"> </a></td>";
+        $content .= "<td><a href='../../util/ajax-mg/deleteMenu.php?item=" . $item . "'><img src=\"../../assets-mg/img/trash.gif\" border=\"0\"> </a></td>";
         $content .= "</tr>";
         $i++;
-    }
-    $menuResult = $menu->getMenu();
-    while ($menuItem = $menuResult->fetch_array()) {
-        $parent = $menuItem['item'];
-        $submenuResult = $menu->getSubmenuByParent($parent);
-        if ($submenuResult) {
-            while ($submenuItem = $submenuResult->fetch_array()) {
-                $index = $submenuItem['index'];
-                $item = $submenuItem['item'];
-                if ($i % 2 == 0) {
-                    $content .= "<tr class='even'>";
-                } else {
-                    $content .= "<tr class='odd'>";
-                }
-                $content .= "<td>" . $index . "</td>";
-                $content .= "<td>" . $item . "</td>";
-                $content .= "<td>" . $parent . "</td>";
-                $content .= "<td><a><img src=\"../../assets-mg/img/edit.png\" border=\"0\"> </a></td>";
-                $content .= "<td><a><img src=\"../../assets-mg/img/trash.gif\" border=\"0\"> </a></td>";
-                $content .= "</tr>";
-                $i++;
-            }
-        }
     }
 }
 echo $content;
