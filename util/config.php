3b29c7ce6f7bbbc711c22364f821e1786b03d467
Bonnenu <283026381@qq.com>
2017-07-30 10:21:43 +0800

    将数据库信息存放在配置文件中

--- util/config.php
+++ util/config.php
@@ -5,6 +5,10 @@
  * Date: 2017/7/27
  * Time: 10:24
  */
+define("SERVERNAME","udm167856273.my3w.com");
+define("USERNAME","udm167856273");
+define("PASSWORD","xwl19900113");
+define("DBNAME","udm167856273_db");
 define("SLIDER_PATH","../../assets/imgs/slider/");
 define("CONTACT_PATH","../../assets/imgs/contact/");
 define("PRODUCT_PATH","../../assets/imgs/product/");
